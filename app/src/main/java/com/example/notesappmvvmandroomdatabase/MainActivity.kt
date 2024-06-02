59d4beb3d8d5cf781a96a48efa70584e58123072
Niraviman-Singha <niraviman15-11689@diu.edu.bd>
2024-06-02 19:53:21 +0600

    I have created function setupViewModel in MainActivity.

--- app/src/main/java/com/example/notesappmvvmandroomdatabase/MainActivity.kt
+++ app/src/main/java/com/example/notesappmvvmandroomdatabase/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.notesappmvvmandroomdatabase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.notesappmvvmandroomdatabase.database.NoteDatabase
+import com.example.notesappmvvmandroomdatabase.model.NoteViewModel
+import com.example.notesappmvvmandroomdatabase.model.NoteViewModelFactory
+import com.example.notesappmvvmandroomdatabase.repository.NoteRepository
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var noteViewModel: NoteViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupViewModel()
+    }
+
+    private fun setupViewModel(){
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application,noteRepository)
+        noteViewModel = ViewModelProvider(this, viewModelProviderFactory)[NoteViewModel::class.java]
     }
 }
